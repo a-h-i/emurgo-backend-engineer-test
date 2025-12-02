b3206fb8edddb65b66619b60ed0af7b5a6336ce7
Ahmed Ismail <ahm3d.hisham@gmail.com>
2025-12-02 00:08:41 +0000

    adds comment about serialization

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,14 @@ import {
 import { z } from "zod";
 const app = new Hono();
 
+// Note: When using serializable isolation level, the db will throw a serialization error if two transactions try to update the same row at the same time.
+// This is because the isolation level is set to serializable by default.
+// This can be handled in many different ways
+// to name a few
+// we can retry the transaction at the api level with backoff and debouncing
+// or we can have whoever is calling the api (assuming some sort of ingest job/queue from a node's data) do the retrying themselves.
+// I felt this was out of scope for this exercise.
+
 app.get("/balance/:address", async (c) => {
   const address = c.req.param("address");
   const db = createDB();
